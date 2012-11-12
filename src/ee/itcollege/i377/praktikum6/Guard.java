40280cdbbaed401cdf1de1e74ae240e23980f17a
Indrek Ots <ots.indrek@gmail.com>
2012-11-12 11:40:18 +0200

    initial commit

---
+++ src/ee/itcollege/i377/praktikum6/Guard.java
@@ -0,0 +1,21 @@
+package ee.itcollege.i377.praktikum6;
+
+public class Guard {
+	
+	private String name;
+	private int age;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+}
---
+++ src/ee/itcollege/i377/praktikum6/GuardController.java
@@ -0,0 +1,33 @@
+package ee.itcollege.i377.praktikum6;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class GuardController {
+	
+	private Logger log = LoggerFactory.getLogger(GuardController.class);
+	
+	@RequestMapping(value="/guardForm", method=RequestMethod.GET)
+	public String guardForm(Model model) {
+		log.debug("Successful guardform request");
+		
+		model.addAttribute("guard", new Guard());
+		
+		return "guard";
+	}
+	
+	@RequestMapping(value="/guardForm", method=RequestMethod.POST)
+	public String guardForm(@ModelAttribute Guard guard, Model model) {
+		log.debug("Adding a new guard. Name: " + guard.getName() + " Age: " + guard.getAge());
+		return "guard";
+	}
+
+
+
+}
